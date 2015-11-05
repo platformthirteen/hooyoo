3e131ad7e150779cbf67c5f8184e801fab03dbc8
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2015-11-04 21:38:41 -0500

    useraccounts:materialize

--- app/packages/hooyoo-lib/package.js
+++ app/packages/hooyoo-lib/package.js
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
     'fourseven:scss@3.3.3_3',
     'poetic:materialize-scss@1.3.5',
     'accounts-facebook@1.0.6',
-    'useraccounts:bootstrap@1.12.4'
+    'useraccounts:materialize@1.12.4'
   ];
 
   api.use(packages);
